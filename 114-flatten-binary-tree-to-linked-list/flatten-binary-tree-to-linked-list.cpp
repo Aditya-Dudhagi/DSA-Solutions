7c496c21eff0940e682eea174be02f94271e7e08
Aditya Dudhagi <141011887+Aditya-Dudhagi@users.noreply.github.com>
2026-02-13 15:29:35 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (9.36%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -10,14 +10,24 @@
  * };
  */
 class Solution {
-public:
-    TreeNode* prev = nullptr;
+public: // iterative solution 
     void flatten(TreeNode* root) {
-        if(root == nullptr) return;
-        flatten(root->right);
-        flatten(root->left);
-        root->right = prev;
-        root->left = nullptr;
-        prev = root;
+        if(!root) return;    
+        // to make it work, we have to connect the right thing to left, so putting right node first in stack will help to connect the right nodes to the left since it is FIFO
+        stack<TreeNode*> st;
+        st.push(root);
+        while(!st.empty()){
+            TreeNode* curr = st.top();
+            st.pop();
+
+            if(curr->right) st.push(curr->right);
+            if(curr->left) st.push(curr->left);
+
+            if(!st.empty()) {
+                curr->right = st.top();
+            }
+
+            curr->left = NULL;
+        }
     }
 };
