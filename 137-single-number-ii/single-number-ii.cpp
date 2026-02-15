6e787bc6c894dc794728556c5788ae6f8a714394
Aditya Dudhagi <141011887+Aditya-Dudhagi@users.noreply.github.com>
2026-02-15 18:40:23 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (86.23%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        // solution 2: check each bit from 0th to 31st for each element and then count it , if it is 1 more than multiple of 3 , then set that index of ans
-        int ans = 0;
-        for(int bitInd = 0; bitInd < 32; bitInd++){
-            int cnt = 0;
-            for(int i=0; i<nums.size(); i++){
-                if(nums[i] & (1<<bitInd)) cnt++;
-            }
-            if(cnt%3 == 1) {
-                ans = ans | (1<<bitInd);
-            }
+        int ones = 0, twos = 0;
+        for(int i: nums){
+            ones = (ones^i)&(~twos);
+            twos = (twos^i)&(~ones);
         }
-        return ans;
+        return ones;
     }
 };
