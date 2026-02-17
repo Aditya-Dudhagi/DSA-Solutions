76a083b9036e02491b8de0373a5191ac75e3f0f9
Aditya Dudhagi <141011887+Aditya-Dudhagi@users.noreply.github.com>
2026-02-17 11:13:35 +0530

    Time: 342 ms (22.27%) | Memory: 302.3 MB (11.19%) - LeetSync

--- 4217-first-element-with-unique-frequency/first-element-with-unique-frequency.cpp
+++ 4217-first-element-with-unique-frequency/first-element-with-unique-frequency.cpp
@@ -5,12 +5,11 @@ public:
         for(int i=0; i<nums.size(); i++){
             hash[nums[i]]++;
         }
-        map<int, int> mp;
+        vector< int> mp(100001, 0);
         for(int i=0; i<hash.size(); i++){
             if(hash[i]>0) mp[hash[i]]++;
-            
         }
-        int ans = -1;
+
         for(int i:nums){
             if(mp[hash[i]] == 1) return i;
         }
