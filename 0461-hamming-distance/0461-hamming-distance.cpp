3341a702fb79f6f29ecc6f7901e68563ffc52020
Aditya Dudhagi <141011887+Aditya-Dudhagi@users.noreply.github.com>
2026-02-21 23:08:37 +0530

    Time: 0 ms (100%), Space: 7.8 MB (66.77%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.cpp
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -1,7 +1,12 @@
 class Solution {
 public:
     int hammingDistance(int x, int y) {
-        return __builtin_popcount(x^y);
-        
+        int cnt = 0;
+        int xorr = x^y;
+        while(xorr != 0){
+            xorr &= xorr - 1 ;
+            cnt++;
+        }
+        return cnt;
     }
 };
