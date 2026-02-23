72225b29dd7b37ac320af6e2a3c98293110259b7
Aditya Dudhagi <141011887+Aditya-Dudhagi@users.noreply.github.com>
2026-02-23 21:47:00 +0530

    Time: 0 ms (100%), Space: 41 MB (5.88%) - LeetHub

--- 3847-find-the-score-difference-in-a-game/3847-find-the-score-difference-in-a-game.cpp
+++ 3847-find-the-score-difference-in-a-game/3847-find-the-score-difference-in-a-game.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     int scoreDifference(vector<int>& nums) {
-        
+        int s1 = 0, s2 = 0;
+        bool p1 = true;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]%2){
+                p1 ^= 1;
+            }
+            if((i+1)%6 == 0){
+                p1 ^= 1;
+            }
+                if(p1) s1+= nums[i];
+                else s2 += nums[i];
+        }
+        return s1-s2;
     }
 };
