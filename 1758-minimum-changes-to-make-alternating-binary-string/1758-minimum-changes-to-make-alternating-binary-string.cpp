f834402b7e4106cd08edbf137d7663860de5d4ec
Aditya Dudhagi <141011887+Aditya-Dudhagi@users.noreply.github.com>
2026-03-06 00:19:08 +0530

    Time: 2 ms (22.27%), Space: 9.3 MB (76.2%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -4,10 +4,11 @@ public:
         int miss1 = 0, miss2 = 0;
         for(int i=0; i<s.size(); i++){
             if(s[i]-'0' != (i%2)) miss1++;
+            else miss2++;
         }
-        for(int i=0; i<s.size(); i++){
-            if(s[i]-'0' != (i+1)%2) miss2++;
-        }
+        // for(int i=0; i<s.size(); i++){
+            
+        // }
         return min(miss1, miss2);
     }
 };
