bef336ae7d8dd2477973a7dd90408ad547b4d3e0
Aditya Dudhagi <141011887+Aditya-Dudhagi@users.noreply.github.com>
2026-04-05 14:48:20 +0530

    Time: 1 ms (60.99%), Space: 10.4 MB (13.45%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool judgeCircle(string moves) {
-        int U = 1, D = -1, R = 1, L = -1;
         int dx = 0, dy = 0;
         for(char ch : moves){
             if(ch == 'U') dy++;
