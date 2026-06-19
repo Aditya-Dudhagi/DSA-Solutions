0d85a9ebdaba138f7cf1de0c1c92fcf1bc9e1d66
Aditya Dudhagi <141011887+Aditya-Dudhagi@users.noreply.github.com>
2026-06-19 10:41:17 +0530

    Time: 131 ms (28%), Space: 34.5 MB (17.87%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.cpp
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
+    unordered_map<string, bool> mp;
     bool solve(string a, string b){
         if(a.compare(b) == 0) return true;
         if(a.length()<= 1) return false;
 
+        string tmp = a + ' ' + b;
+        if(mp.find(tmp) != mp.end()) return mp[tmp];
+
         int n = a.length();
         bool flag = false;
+        
+        // swap & no swap
         for(int i=1; i<=n-1; i++){
             if((solve(a.substr(0, i), b.substr(n-i, i)) && solve(a.substr(i, n-i), b.substr(0,n-i))) || 
                 (solve(a.substr(0, i), b.substr(0, i)) && solve(a.substr(i, n-i), b.substr(i,n-i)))){
@@ -14,7 +20,7 @@ public:
                 }
             
         }
-        return flag;
+        return mp[tmp] = flag;
     }
     bool isScramble(string s1, string s2) {
         return solve(s1, s2);
