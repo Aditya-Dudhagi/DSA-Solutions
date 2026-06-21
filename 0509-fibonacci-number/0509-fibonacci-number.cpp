ac14f3d1b653a4a92808f24162fb3705c06b80fd
Aditya Dudhagi <141011887+Aditya-Dudhagi@users.noreply.github.com>
2026-06-21 19:10:43 +0530

    Time: 2 ms (78.38%), Space: 7.6 MB (88.09%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,8 +1,16 @@
 class Solution {
 public:
-    int fib(int n) {
-        if(n <= 0) return 0;
-        if(n == 1) return 1;
-        return fib(n-1) + fib(n-2);
+    int t[31];
+    Solution(){
+        memset(t, -1, sizeof(t));
     }
+    int solve(int n){
+        if(n<=1) return n;
+        if(t[n] != -1) return t[n];
+        return t[n] = solve(n-1) + solve(n-2);
+    }
+    int fib(int n) {
+        return solve(n);
+    }
+
 };
