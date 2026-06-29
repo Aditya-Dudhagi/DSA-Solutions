c8a14de98aba7f831815f8d4a5dba3dafb708b50
Aditya Dudhagi <141011887+Aditya-Dudhagi@users.noreply.github.com>
2026-06-29 23:05:52 +0530

    Time: 1 ms (61.56%), Space: 37.3 MB (47.8%) - LeetHub

---
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    stack<TreeNode*> stb, stn;
+    void pushAllb(TreeNode* node){
+        for(; node != NULL; stb.push(node), node = node->right);
+    }
+    void pushAlln(TreeNode* node){
+        for(; node != NULL; stn.push(node), node = node->left);
+    }
+    int before(){
+       TreeNode* tmp = stb.top();
+       stb.pop();
+       pushAllb(tmp->left); 
+       return tmp->val;
+    }
+    int next(){
+       TreeNode* tmp = stn.top();
+       stn.pop();
+       pushAlln(tmp->right); 
+       return tmp->val;
+    }
+    bool findTarget(TreeNode* root, int k) {
+        bool flag = false;
+        pushAllb(root);
+        pushAlln(root);
+        int i = next(), j = before();
+        while(!stn.empty() && !stb.empty()){
+            if(i+j == k && i!=j) {
+                flag = true;
+                break;
+            }
+            else if(i+j<k){
+                i=next();
+            } else j=before();
+        }
+        return flag;
+    }
+};
