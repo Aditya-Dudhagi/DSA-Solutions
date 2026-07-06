45c5ec1620a9a4bb25e1206771f19b24a9c9c359
Aditya Dudhagi <141011887+Aditya-Dudhagi@users.noreply.github.com>
2026-07-06 13:52:32 +0530

    Time: 16 ms (5.45%), Space: 17 MB (40.18%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,60 @@
+class Pair{
+    public:
+    int x, y, t;
+    Pair(int r, int c, int t){
+        x = r, y = c, this->t = t;
+    }
+};
+
+class Solution {
+public:
+    int bfs(vector<vector<int>>& vis, vector<vector<int>>& grid, int dx[], int dy[]){
+        int n = grid.size(), m = grid[0].size();
+        queue<Pair> q;
+        int time = 0;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j] == 2){
+                    q.push(Pair(i, j, 0));
+                }
+            }
+        }
+
+        while(!q.empty()){
+            int row = q.front().x, col = q.front().y;
+            time = q.front().t;
+
+            q.pop();
+            for(int i=0; i<4; i++){
+            int nrow = row + dx[i], ncol = col + dy[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol] == 1 && vis[nrow][ncol]!=2){
+                    vis[nrow][ncol] = 2;
+                    cout<<nrow<<" "<<ncol<<" "<<time+1<<endl;
+                    q.push(Pair(nrow, ncol, time+1));
+                }
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(vis[i][j] == 1){
+                    return -1;
+                }
+            }
+        }
+        return time;
+    }
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<int>> vis = grid;
+        int dx[] = {-1, 0 , 1, 0};
+        int dy[] = { 0 , 1, 0, -1};
+
+
+        return bfs(vis, grid, dx, dy);
+
+    }
+};
