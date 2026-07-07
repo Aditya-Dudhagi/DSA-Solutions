5e8e4e2fa5321924b5e5864b44d1e2a705a02241
Aditya Dudhagi <141011887+Aditya-Dudhagi@users.noreply.github.com>
2026-07-07 18:01:24 +0530

    Time: 17 ms (62.44%), Space: 39 MB (20.23%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -9,15 +9,17 @@ class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int m = mat.size(), n = mat[0].size();
-        vector<vector<int>> vis = mat;
+        vector<vector<int>> vis(m, vector<int>(n, 0));
         vector<vector<int>> ans(m, vector<int>(n, 0));
         
-        // 0 means visited , 1 means not visited
         queue<Pair> q;
 
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(mat[i][j] == 0) q.push(Pair(i, j, 0));   
+                if(mat[i][j] == 0){
+                    q.push(Pair(i, j, 0));   
+                    vis[i][j] = 1;
+                }
                              
             }
         }
@@ -31,8 +33,8 @@ public:
 
             for(int i=0; i<4; i++){
                 int nrow = row + dx[i], ncol = col + dy[i];
-                if(nrow>=0 && nrow <m && ncol>=0 && ncol <n && mat[nrow][ncol] == 1 && vis[nrow][ncol] != 0){
-                    vis[nrow][ncol] = 0;
+                if(nrow>=0 && nrow <m && ncol>=0 && ncol <n && mat[nrow][ncol] == 1 && vis[nrow][ncol] == 0){
+                    vis[nrow][ncol] = 1;
                     ans[nrow][ncol] = dis+1;
                     q.push(Pair(nrow, ncol, dis+1));
 
