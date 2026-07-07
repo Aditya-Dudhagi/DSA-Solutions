e95011257e2cb3bdab4ac5ac75e102efb090b06f
Aditya Dudhagi <141011887+Aditya-Dudhagi@users.noreply.github.com>
2026-07-08 00:21:35 +0530

    Time: 14 ms (22.09%), Space: 31.8 MB (71.44%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,35 +1,44 @@
 class Solution {
 public:
-    void dfs(int row, int col, vector<vector<int>>& vis, vector<vector<int>>& grid, int dx[], int dy[]){
-        vis[row][col] = 1;
-        int n = grid.size(), m = grid[0].size();
-        for(int i=0; i<4; i++){
-            int nrow = row + dx[i], ncol = col + dy[i];
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol] == 1 && !vis[nrow][ncol]){
-                dfs(nrow, ncol, vis, grid, dx, dy);
-            }
-        }
-    }
     int numEnclaves(vector<vector<int>>& grid) {
         int n=grid.size() , m = grid[0].size();
         vector<vector<int>> vis(n, vector<int> (m, 0));
 
         int dx[] = {-1, 0, 1, 0}, dy[] = {0, 1, 0, -1};
+        queue<pair<int, int>> q;
+
         
         for(int j=0; j<m; j++){
             if(grid[0][j] == 1 && !vis[0][j]){
-                dfs(0, j, vis, grid, dx, dy);
+                q.push({0, j});
+                vis[0][j] = 1;
             }
             if(grid[n-1][j] == 1 && !vis[n-1][j]){
-                dfs(n-1, j, vis, grid, dx, dy);
+                q.push({n-1, j});
+                vis[n-1][j] = 1;
             }
         }
         for(int i=0; i<n; i++){
             if(grid[i][0] == 1 && !vis[i][0]){
-                dfs(i, 0, vis, grid, dx, dy);
+                q.push({i, 0});
+                vis[i][0] = 1;
             }
             if(grid[i][m-1] == 1 && !vis[i][m-1]){
-                dfs(i, m-1, vis, grid, dx, dy);
+                q.push({i, m-1});
+                vis[i][m-1] = 1;
+            }
+        }
+
+        while(!q.empty()){
+            int row = q.front().first, col = q.front().second;
+            q.pop();
+
+            for(int i=0; i<4; i++){
+                int nrow = row + dx[i], ncol = col + dy[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol] == 1 && !vis[nrow][ncol]){
+                    q.push({nrow, ncol});
+                    vis[nrow][ncol] = 1;
+                }
             }
         }
 
