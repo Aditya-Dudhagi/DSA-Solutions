7c717a986083866b2e8c8fba5d4f0b7da16de9f4
Aditya Dudhagi <141011887+Aditya-Dudhagi@users.noreply.github.com>
2026-07-09 18:20:24 +0530

    Time: 10 ms (5.09%), Space: 17.4 MB (58.87%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
-    bool bfs(int start, vector<vector<int>>& graph, vector<int> &color){
-        queue<int> q;
-        q.push(start);
-        color[start] = 0;
-
-        while(!q.empty()){
-            int node = q.front();
-            cout<<"Node: "<<node<<" "<<color[node]<<endl;
-            q.pop();
-
-            for(int adjnode : graph[node]){
-                // if adj node is not colored, then give it opposite color and push into queue
-                if(color[adjnode] == -1){
-                    color[adjnode] = !color[node];
-                    cout<<"Coloring adj node "<<adjnode<<" by color: "<<color[adjnode]<<" & push "<<adjnode<<endl;
-                    q.push(adjnode);
+    bool dfs(int start, int color, vector<vector<int>>& graph, vector<int> &colorMat){
+        colorMat[start] = color;
+    
+            for(int adjnode : graph[start]){
+                // if adj node is not colored, then give it opposite colorMat and push into queue
+                if(colorMat[adjnode] == -1){
+                    if(dfs(adjnode, !color, graph , colorMat) == false) return false;
+                    cout<<"Coloring adj node "<<adjnode<<" by colorMat: "<<colorMat[adjnode]<<" & push "<<adjnode<<endl;
                 } 
-                // if adj node has same color then someone have colored it from other path
-                else if(color[adjnode] == color[node]){
+                // if adj node has same colorMat then someone have colored it from other path
+                else if(colorMat[adjnode] == colorMat[start]){
                     cout<<"BINGOOO you found someone already colored , that is node: "<<adjnode<<endl;
                     return false;
                 }
             }
-        }
         return true;
     }
     bool isBipartite(vector<vector<int>>& graph) {
@@ -32,7 +23,7 @@ public:
 
         for(int i=0; i<V; i++){
             if(color[i] == -1){
-                if(bfs(i, graph, color) == false) return false;
+                if(dfs(i,0, graph, color) == false) return false;
             } 
         }        
         return true;
