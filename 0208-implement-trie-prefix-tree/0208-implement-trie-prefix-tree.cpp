9b25117696194defc784c2b0fe4c84f8fe166d83
Aditya Dudhagi <141011887+Aditya-Dudhagi@users.noreply.github.com>
2026-08-11 10:28:44 +0530

    Time: 20 ms (58.77%), Space: 50.4 MB (86.96%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -1,64 +1,66 @@
-struct Node {
-    Node *links[26];
+struct Node{
+    Node* links[26];
     bool flag;
+
     bool containsKey(char ch){
-        return (links[ch - 'a'] != NULL);
+        return links[ch-'a'] != NULL;
     }
 
-    void put(char ch, Node * node){
-        links[ch - 'a'] = node;
+    void put(char ch, Node* node){
+        links[ch-'a'] = node;
     }
 
     Node* get(char ch){
         return links[ch-'a'];
     }
 
-    void setEnd(){
-        flag = true;
-    }
-
     bool isEnd(){
         return flag;
     }
 
+    void setEnd(){
+        flag = true;
+    }
+
 };
+
 class Trie {
 private:
-    Node *root;
+Node* root;
 public:
     Trie() {
         root = new Node();
     }
     
     void insert(string word) {
-        Node *node = root;
-        for(int i=0; i<word.size(); i++){
-            if(!node->containsKey(word[i])){
-                node->put(word[i], new Node());
+        Node* node = root;
+        for(char ch: word){
+            if(!node->containsKey(ch)){
+                node->put(ch, new Node());
             }
-            node = node->get(word[i]);    
+            node = node->get(ch);
         }
         node->setEnd();
     }
     
-    bool search(string word) {
+    bool search(string prefix) {
         Node* node = root;
-        for(int i=0; i<word.size(); i++){
-            if(!node->containsKey(word[i])){
-                return false;
+        for(char ch : prefix){
+            if(node->containsKey(ch)){
+                node = node->get(ch);
             }
-            node = node->get(word[i]);
+            else return false;
         }
-        return node->isEnd();            
+        return node->isEnd();
     }
     
     bool startsWith(string prefix) {
         Node* node = root;
-        for(int i=0; i<prefix.size(); i++){
-            if(!node->containsKey(prefix[i])){
-                return false;
+        for(char ch : prefix){
+            if(node->containsKey(ch)){
+                node = node->get(ch);
             }
-            node = node->get(prefix[i]);
+            else return false;
         }
         return true;
     }
