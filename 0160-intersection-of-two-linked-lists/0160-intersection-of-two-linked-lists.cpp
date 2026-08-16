f3ddd389ba6330de6d8c9755faa50e65d3ef301e
Aditya Dudhagi <141011887+Aditya-Dudhagi@users.noreply.github.com>
2026-08-16 22:56:36 +0530

    Time: 53 ms (76.88%), Space: 24 MB (25.75%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -9,39 +9,18 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        int n1 = 1, n2 = 1;
-        ListNode *tmp = headA, *tmpa = headA, *tmpb = headB;
-        while(tmp!=nullptr){
-            tmp=tmp->next;
-            n1++;
-        }
+        if(!headA || !headB) return NULL;
 
-        tmp = headB;
-        while(tmp!=nullptr){
-            tmp = tmp->next;
-            n2++;
-        }
-
-        int diff =abs(n2 - n1);
-        if(n2>n1){
-            while(diff>0){
-                tmpb = tmpb->next;
-                diff--;
-            }
-        }
-        else{
-            while(diff>0){
-                tmpa = tmpa->next;
-                diff--;
-            }
-        }
-
-        while(tmpa && tmpb){
-            if(tmpa == tmpb) return tmpa;
-
+        ListNode *tmpa = headA, *tmpb = headB;
+        while(tmpa != tmpb){
             tmpa = tmpa->next;
             tmpb = tmpb->next;
+
+            if(tmpa == tmpb) return tmpa;
+
+            if(tmpa == NULL) tmpa = headB;
+            if(tmpb == NULL) tmpb = headA;
         }
-        return nullptr;
+        return tmpa;
     }
 };
