bc8ae1a960be9533f15022de2a3e37a73a3dbf43
Aditya Dudhagi <141011887+Aditya-Dudhagi@users.noreply.github.com>
2026-09-04 12:16:29 +0530

    Time: 0 ms (100%), Space: 18.4 MB (30.5%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
-    void dfs(int row, int col, vector<vector<int>>& ans, vector<vector<int>>& image, int inicolor, int newcolor, int dx[], int dy[]){
-        int n = image.size(), m = image[0].size();
-        ans[row][col] = newcolor;
-        for(int i=0; i<4; i++){
-            int nrow = row + dx[i], ncol = col + dy[i];
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol] == inicolor && ans[nrow][ncol]!=newcolor){
-                ans[nrow][ncol] = newcolor;
-                dfs(nrow, ncol, ans, image, inicolor, newcolor, dx, dy);
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int m = image.size(), n = image[0].size();
+        vector<vector<int>> vis = image;
+        int iniColor = image[sr][sc];
+        queue<pair<int, int>> q;
+
+        int dx[] = {-1, 0, 1, 0};
+        int dy[] = { 0, 1, 0, -1};
+
+        q.push({sr, sc});
+
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+
+            int row = it.first, col = it.second;
+            if(image[row][col] == iniColor && vis[row][col] != color) vis[row][col] = color;
+
+            for(int i=0; i<4; i++){
+                int nrow = row + dx[i] , ncol = col + dy[i];
+
+                if(nrow>=0 && nrow<m && ncol >= 0 && ncol<n && image[nrow][ncol] == iniColor &&  vis[nrow][ncol] != color){
+                    vis[nrow][ncol] = color;
+                    q.push({nrow, ncol});
+                }
             }
         }
-    }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int inicolor = image[sr][sc];
-        vector<vector<int>> ans = image;
-        int dx[] = {-1, 0, 1, 0};
-        int dy[] = {0, 1, 0, -1};
-        dfs(sr, sc, ans, image, inicolor, color, dx, dy);
-        return ans;
+
+        return vis;
     }
 };
