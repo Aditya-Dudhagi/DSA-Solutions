93a41e914aeb9ab571c1f594de2106e7d02ac603
Aditya Dudhagi <141011887+Aditya-Dudhagi@users.noreply.github.com>
2026-09-05 23:31:36 +0530

    Time: 0 ms (100%), Space: 14.3 MB (68.03%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,39 +1,44 @@
 class Solution {
 public:
-    void dfs(int row, int col, vector<vector<int>> &vis, vector<vector<char>> board, int dx[], int dy[]){
-        vis[row][col] = 0;
+    void dfs(int row, int col, vector<vector<int>> & vis, vector<vector<char>> &board, int dx[], int dy[]){
+        vis[row][col] = 1;
         int n = board.size(), m = board[0].size();
         for(int i=0; i<4; i++){
             int nrow = row + dx[i], ncol = col + dy[i];
-            if(nrow>=0 && nrow<n && ncol >=0 && ncol<m && board[nrow][ncol] == 'O' && vis[nrow][ncol] != 0){
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && board[nrow][ncol]=='O' && vis[nrow][ncol]!=1){
                 dfs(nrow, ncol, vis, board, dx, dy);
             }
-        }
+        }    
     }
+
     void solve(vector<vector<char>>& board) {
         int n = board.size(), m = board[0].size();
-        vector<vector<int>> vis (n, vector<int> (m, 1));
-        // 0 means cant visit, 1 means can visit
+        vector<vector<int>> vis(n, vector<int> (m, 0));
+
         int dx[] = {-1, 0, 1, 0};
         int dy[] = {0, 1, 0, -1};
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(board[i][j] == 'X'){
-                    vis[i][j] = 0;
-                }
+        for(int j=0; j<m; j++){
+            if(!vis[0][j] && board[0][j] == 'O'){
+                dfs(0, j, vis, board, dx, dy);
+            }
+            if(!vis[n-1][j] && board[n-1][j] == 'O'){
+                dfs(n-1, j, vis, board, dx, dy);
             }
         }
+
         for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if((i==0 || j==0 || i==n-1 || j==m-1) && board[i][j] == 'O'){
-                    dfs(i, j, vis, board, dx, dy);
-                }
+            if(!vis[i][0] && board[i][0] == 'O'){
+                dfs(i, 0, vis,board, dx, dy);
+            }
+            if(!vis[i][m-1] && board[i][m-1] == 'O'){
+                dfs(i, m-1, vis, board, dx, dy);
             }
         }
+
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(vis[i][j] == 1){
+                if(vis[i][j] == 0 && board[i][j] == 'O'){
                     board[i][j] = 'X';
                 }
             }
