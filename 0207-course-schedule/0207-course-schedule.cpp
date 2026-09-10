6ba9a1a5931bbeb6c090e2b7d00dd03bd2d6696a
Aditya Dudhagi <141011887+Aditya-Dudhagi@users.noreply.github.com>
2026-09-10 16:18:55 +0530

    Time: 10 ms (24.98%), Space: 19.6 MB (46.76%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        // using topo sort
-        int V = numCourses;
-        vector<int> indeg(V, 0);
-        vector<int> adj[V];
-        for(int i=0; i<prerequisites.size(); i++){
-            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
-            indeg[prerequisites[i][0]]++;
-        }   
+        // topo sort
+        int v = numCourses;
+        vector<int> indeg(v, 0);
+
+        vector<int> adj[v];
+
+        for(auto it : prerequisites){
+            adj[it[1]].push_back(it[0]);
+            indeg[it[0]]++;
+        }
 
         queue<int> q;
-        for(int i=0; i<V; i++){
-            if(indeg[i]==0) q.push(i);
+        for(int i=0; i<v; i++){
+            if(indeg[i] == 0) q.push(i);
         }
 
         int cnt = 0;
         while(!q.empty()){
             int node = q.front();
             q.pop();
-            cnt++;
+            cnt ++;
 
-            for(int it: adj[node]){
-                indeg[it] --;
+            for(auto it : adj[node]){
+                indeg[it]--;
                 if(indeg[it] == 0) q.push(it);
             }
         }
 
-        return cnt == V;
+        return cnt == v;
     }
 };
