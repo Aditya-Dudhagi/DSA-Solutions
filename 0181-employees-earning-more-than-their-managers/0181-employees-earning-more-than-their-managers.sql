13031f53e5651d5233744499129cfaeb84ab0d84
Aditya Dudhagi <141011887+Aditya-Dudhagi@users.noreply.github.com>
2026-09-16 10:02:02 +0530

    Time: 979 ms (7.1%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -2,7 +2,6 @@
 select name as Employee
 from Employee e
 where salary > (
-select salary
-from Employee 
-where id = e.managerId 
-);
+    select salary from Employee
+    where id = e.managerId
+)
